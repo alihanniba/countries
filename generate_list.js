36bbbcebc8ecc81311ab9bc539e92348c6a3a2f5
Bez Reyhan <bezreyhan@gmail.com>
2015-07-27 17:14:45 -0700

    reformat to include country name in country code

--- generate_list.js
+++ generate_list.js
@@ -3,7 +3,10 @@ var countries = require('./countries.json');
 
 var filteredCountries = countries
   .map(function(country) {
-    return {label: country.name.common, value: country.callingCode[0]};
+    var name = country.name.common;
+    return {
+      label: name,
+      value: name + '-' + country.callingCode[0]};
   })
   // only return countries that have a calling code
   .filter(function(country) {
